f0131cc2f3dac81a796f34404edb029981391cb5
Stephan Rempel <28607147+StephanRempel@users.noreply.github.com>
2023-06-10 18:08:47 +0200

    restored

--- show_tree.py
+++ show_tree.py
@@ -3,25 +3,25 @@ import sys
 
 
 def print_tree(startpath, filesonly=False, dirsonly=False):
-    print(f"├ {os.path.basename(startpath)}")
+    print(f"+ {os.path.basename(startpath)}")
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, "").count(os.sep)
         indent = "|   " * level
-        print(f"{indent}├──{os.path.basename(root)}/")
+        print(f"{indent}+--{os.path.basename(root)}/")
         if filesonly:
             dirs.clear()  # Suppress the output of subdirectories
         elif dirsonly:
             files.clear()  # Suppress the output of files
         for file in files:
             subindent = "|   " * (level + 1)
-            print(f"{subindent}├──{file}")
+            print(f"{subindent}+--{file}")
         for dir in dirs:
             subindent = "|   " * (level + 1)
-            print(f"{subindent}├──{dir}/")
+            print(f"{subindent}+--{dir}/")
 
 
 if __name__ == "__main__":
-    theDir = "..\.."
+    theDir = "."
     if len(sys.argv) > 1:
         theDir = sys.argv[1]
 
